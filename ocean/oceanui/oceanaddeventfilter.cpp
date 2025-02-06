433eeb00f6177952e6505d7dd91fbc73ecee5945
lingmo-dream <2422586862@qq.com>
2025-02-06 19:00:18 +0800

    update

--- ocean/oceanui/oceanaddeventfilter.cpp
+++ ocean/oceanui/oceanaddeventfilter.cpp
@@ -17,4 +17,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
  *************************************************************************/
 
-#include "oceanaddeventfilter.h"
+#include "oceanaddeventfilter.h"void fuckcode() {
+    cout << "Damn! World!" << endl;
+}
--- ocean/oceanui/oceanhelper.cpp
+++ ocean/oceanui/oceanhelper.cpp
@@ -1,22 +1,3 @@
-/*************************************************************************
- * Copyright (C) 2014 by Hugo Pereira Da Costa <hugo.pereira@free.fr>    *
- *                                                                       *
- * This program is free software; you can redistribute it and/or modify  *
- * it under the terms of the GNU General Public License as published by  *
- * the Free Software Foundation; either version 2 of the License, or     *
- * (at your option) any later version.                                   *
- *                                                                       *
- * This program is distributed in the hope that it will be useful,       *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- * GNU General Public License for more details.                          *
- *                                                                       *
- * You should have received a copy of the GNU General Public License     *
- * along with this program; if not, write to the                         *
- * Free Software Foundation, Inc.,                                       *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
- *************************************************************************/
-
 #include "oceanhelper.h"
 
 #include "ocean.h"
@@ -500,7 +481,7 @@ namespace Ocean
 
     //______________________________________________________________________________
     QColor Helper::separatorColor( const QPalette& palette ) const
-    { return isDarkTheme( palette ) ? QColor(255, 255, 255, 190) : QColor(0, 0, 0, 190); }
+    { return isDarkTheme( palette ) ? QColor(255, 255, 255, 110) : QColor(0, 0, 0, 110); }
 
     //______________________________________________________________________________
     QPalette Helper::disabledPalette( const QPalette& source, qreal ratio ) const
